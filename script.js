945ed0d897273da0686c065be57949f03f16f339
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-06-28 10:35:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+const paragraph = document.getElementById("paragraph");
+const link = document.getElementById("link");
 function highlight() {
-    //Write your code here
-
-
+    let boldWords = paragraph.getElementByTagName("strong");
+	 for(let i=0; i<boldWords.length; i++){
+		 boldWords[i].style.color="green";
+	 }
 }
-
-
 function return_normal() {
-    //Write your code here
-
-    
+    let boldWords = paragraph.getElementByTagName("strong");
+	for(let i=0; i<boldWords.length; i++){
+		boldWords[i].style.color = "black";
+	}
 }
+link.addEventListener("mouseover", highlight);
+link.addEventListener("mouseout", normal);
